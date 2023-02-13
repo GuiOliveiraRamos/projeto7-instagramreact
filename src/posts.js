3c7a48806f28df420d4db16220a7f646601ed06d
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-02-13 08:43:52 -0300

    projeto finalizado(eu acho)

--- src/posts.js
+++ src/posts.js
@@ -3,14 +3,14 @@ import { useState } from "react"
 export default function Posts() {
 
     const post = [
-        { img: "assets/img/meowed.svg", text: "meowed", postImg: "assets/img/gato-telefone.svg", likeImg: "assets/img/respondeai.svg", likePage: "respondeai" },
-        { img: "assets/img/barked.svg", text: "barked", postImg: "assets/img/dog.svg", likeImg: "assets/img/adorable_animals.svg", likePage: "adorable_animals" },
-        { img: "assets/img/meowed.svg", text: "test", postImg: "assets/img/gato-telefone.svg", likeImg: "assets/img/respondeai.svg", likePage: "respondeai" }
+        { img: "assets/img/meowed.svg", text: "meowed", postImg: "assets/img/gato-telefone.svg", likeImg: "assets/img/respondeai.svg", likePage: "respondeai", likes: 950 },
+        { img: "assets/img/barked.svg", text: "barked", postImg: "assets/img/dog.svg", likeImg: "assets/img/adorable_animals.svg", likePage: "adorable_animals", likes: 584 },
+        { img: "assets/img/meowed.svg", text: "test", postImg: "assets/img/gato-telefone.svg", likeImg: "assets/img/respondeai.svg", likePage: "respondeai", likes: 57 }
     ]
 
     return (
         <div class="posts">
-            {post.map((p) => <Post img={p.img} text={p.text} postImg={p.postImg} likeImg={p.likeImg} likePage={p.likePage} />)}
+            {post.map((p) => <Post img={p.img} text={p.text} postImg={p.postImg} likeImg={p.likeImg} likePage={p.likePage} likes={p.likes} />)}
         </div>
     )
 }
@@ -20,6 +20,16 @@ function Post(props) {
 
     const [save, setSave] = useState(false)
     const [like, setLike] = useState(false)
+    const [contador, setContador] = useState(props.likes)
+
+    const likes = () => {
+        setLike(!like)
+        if (!like === true) {
+            setContador(contador + 1)
+        } else {
+            setContador(contador - 1)
+        }
+    }
 
     return (
         <div data-test="post" class="post">
@@ -40,7 +50,7 @@ function Post(props) {
             <div className="fundo">
                 <div className="acoes">
                     <div>
-                        <ion-icon data-test="like-post" onClick={() => setLike(!like)} class={!like ? "normal" : "vermelho"} name={!like ? "heart-outline" : "heart"}></ion-icon>
+                        <ion-icon data-test="like-post" onClick={likes} class={!like ? "normal" : "vermelho"} name={!like ? "heart-outline" : "heart"}></ion-icon>
                         <ion-icon name="chatbubble-outline"></ion-icon>
                         <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
@@ -51,8 +61,8 @@ function Post(props) {
 
                 <div className="curtidas">
                     <img src={props.likeImg} alt="respondeai" />
-                    <div className="texto">
-                        Curtido por <strong>{props.likePage}</strong> e <strong>outras 101.523 pessoas</strong>
+                    <div className="texto" data-test="likes-number">
+                        Curtido por <strong>{props.likePage}</strong> e <strong>outras {contador} pessoas</strong>
                     </div>
                 </div>
             </div>
